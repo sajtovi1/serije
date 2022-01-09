b4777f2e3a867df4e3bead4d11f50aee58793112
MarkoRuzic <ruskegdin23@gmail.com>
2022-01-09 15:36:48 +0100

    rute

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('registracija', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('epizode', \App\Http\Controllers\API\EpizodeController::class);
+    Route::resource('sezone', \App\Http\Controllers\API\SezonaController::class);
+});
